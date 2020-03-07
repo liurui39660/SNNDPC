c8e5db1e742f5bb538cc76a7f1a8e0ac97764c7f
Rui LIU <xxliuruiabc@gmail.com>
2020-03-08 01:41:12 +0800

    Specify average method to avoid future warning message.

--- util/EvaluateAssignment.py
+++ util/EvaluateAssignment.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
 	if len(sys.argv) < 3: exit(1)
 	truth = np.loadtxt(sys.argv[1], int)
 	predict = np.loadtxt(sys.argv[2], int)
-	print(f"AMI = {adjusted_mutual_info_score(truth, predict):.4f}")
+	print(f"AMI = {adjusted_mutual_info_score(truth, predict, average_method='max'):.4f}")
 	print(f"ARI = {adjusted_rand_score(truth, predict):.4f}")
 	print(f"FMI = {fowlkes_mallows_score(truth, predict):.4f}")
